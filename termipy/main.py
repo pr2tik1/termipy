6de8ce9ead82bcb7345cb7e9f0694fb5f7e37d9a
Pratik Kumar <pr2tik1@gmail.com>
2024-10-09 08:02:56 +0000

    update to if else

--- termipy/main.py
+++ termipy/main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import readline
-from datetime import datetime
 from termipy.utils import *
 from termipy.env_setup import handle_setpyenv, handle_setrenv
 from termipy.docs import welcome_message, help_message
@@ -16,70 +15,71 @@ def handle_input(command):
         return True
     cmd, *args = parts
     try:
-        match cmd:
-            case "exit":
-                return False
-            case "echo":
-                sys.stdout.write(f"{' '.join(args)}\n")
-            case "getwd":
-                sys.stdout.write(f"{os.getcwd()}\n")
-            case "setwd":
-                handle_cd(args, command)
-            case "typeof":
-                handle_type(args, command)
-            case "clr" | "cls" | "clear":
-                os.system('cls' if os.name == 'nt' else 'clear')
-            case "tree":
-                handle_tree(args, command)
-            case "makedir":
-                handle_mkdir(args, command)
-            case "ff":
-                if args:
-                    find_file(args[0])
-                else:
-                    sys.stdout.write("Error: No filename specified for find_file command.\n")
-            case "setpyenv":
-                handle_setpyenv(args, command)
-            case "setrenv":
-                handle_setrenv(args, command)
-            case "help":
-                sys.stdout.write(help_message)
-            case "about":
-                if args:
-                    get_file_details(args[0])
-                else:
-                    sys.stdout.write("Error: No file specified for about command.\n")
-            case "commands":
-                list_builtins_and_executables()
-            case "delete":
-                handle_delete(args, command)
-            case "search":
-                handle_search(args, command)
-            case "create":
-                handle_create(args, command)
-            case "view":
-                handle_view(args, command)
-            case "rename":
-                handle_rename(args, command)
-            case "diskusage":
-                display_disk_usage(args)
-            case "permissions":
-                check_permissions(args)
-            case _:
-                sys.stdout.write(f"{cmd}: command not found\n")
+        if cmd == "exit":
+            return False
+        elif cmd == "echo":
+            sys.stdout.write(f"{' '.join(args)}\n")
+        elif cmd == "getwd":
+            sys.stdout.write(f"{os.getcwd()}\n")
+        elif cmd == "setwd":
+            handle_cd(args, command)
+        elif cmd == "typeof":
+            handle_type(args, command)
+        elif cmd in ["clr", "cls", "clear"]:
+            os.system('cls' if os.name == 'nt' else 'clear')
+        elif cmd == "tree":
+            handle_tree(args, command)
+        elif cmd == "makedir":
+            handle_mkdir(args, command)
+        elif cmd == "ff":
+            if args:
+                find_file(args[0])
+            else:
+                sys.stdout.write("Error: No filename specified for find_file command.\n")
+        elif cmd == "setpyenv":
+            handle_setpyenv(args, command)
+        elif cmd == "setrenv":
+            handle_setrenv(args, command)
+        elif cmd == "help":
+            sys.stdout.write(help_message)
+        elif cmd == "about":
+            if args:
+                get_file_details(args[0])
+            else:
+                sys.stdout.write("Error: No file specified for about command.\n")
+        elif cmd == "commands":
+            list_builtins_and_executables()
+        elif cmd == "delete":
+            handle_delete(args, command)
+        elif cmd == "search":
+            handle_search(args, command)
+        elif cmd == "create":
+            handle_create(args, command)
+        elif cmd == "view":
+            handle_view(args, command)
+        elif cmd == "rename":
+            handle_rename(args, command)
+        elif cmd == "diskusage":
+            display_disk_usage(args)
+        elif cmd == "permissions":
+            check_permissions(args)
+        else:
+            sys.stdout.write(f"{cmd}: command not found\n")
     except Exception as e:
         sys.stdout.write(f"Error processing command: {str(e)}\n")
     return True
+
 
 def main():
     sys.stdout.write(welcome_message)
     sys.stdout.flush()
     prompt = "@termipy >> "
-    
+
     while True:
         command = input(prompt)
         if not handle_input(command):
             break
+
 
 if __name__ == "__main__":
     main()

f21b2424674b5d00b0f6e59348a89f80df2cc379
Pratik Kumar <pr2tik1@gmail.com>
2024-10-22 11:34:18 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'termipy=termipy.main:main',
+            'termipy=termipy.main',
         ],
     },
     classifiers=[

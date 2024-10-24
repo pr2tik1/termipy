0faf69f072c8482045affce1ba5b1f8b8ce58782
pr2tik1 <pr2tik1@gmail.com>
2024-10-24 15:50:50 +0530

    0.2.7

--- termipy/docs.py
+++ termipy/docs.py
@@ -12,7 +12,7 @@ Welcome to TermiPy!
 Type 'help' for a list of available commands and their usage.
 
 Author: Pratik Kumar
-Version: 0.2.6
+Version: 0.2.7
 Date: 2024-10-24
 Contact: pr2tik1@gmail.com
 """
@@ -26,7 +26,7 @@ File Operations:
   search     - Search for files and directories
   delete     - Delete a file or directory
   rename     - Rename a file or directory
-  permissions- Change permissions of a file or directory
+  permissions- Check permissions of a file or directory
 
 System Commands:
   echo       - Print a message to the console
@@ -34,12 +34,13 @@ System Commands:
   ls         - List directory contents
   setwd      - Change the current working directory
   typeof     - Display the type of a file or directory
-  clear      - Clear the console screen
+  clear      - Clear the console screen (aliases: cls, clr)
   diskusage  - Display disk usage information
 
 Environment Setup:
   setpyenv   - Set up a Python virtual environment
   setrenv    - Set up an R environment
+  createdevcontainer - Create a devcontainer (Remote Containers Extension for VSCode)
 
 Utility Commands:
   help       - Display this help message
@@ -47,7 +48,7 @@ Utility Commands:
   commands   - List all available commands
 
 Resource Monitoring:
-  resource   - Display system resource usage information
+  resource   - Display system resource usage information (aliases: resources, stats)
 
 Other:
   exit       - Exit the TermiPy shell
